5195d227d56511578e51e47526c938de662e2bc9
Anton <antonmett@gmail.com>
2022-04-14 17:17:49 +0300

    little work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +30,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatSortModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shipment/shipment.component.ts
+++ src/app/shipment/shipment.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+
 
 import { Shipment } from './shipment.model';
 import { ShipmentsService } from './shipments.service';
@@ -11,6 +14,12 @@ import { ShipmentsService } from './shipments.service';
 export class ShipmentComponent {
 
   shipmentsArray: Shipment[] = [];
+  loading: boolean = false;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
 
   columnsToDisplay: string[] = [
     'orderNo',
@@ -18,16 +27,48 @@ export class ShipmentComponent {
     'customer',
     'trackingNo',
     'status',
-    'consignee'
+    'consignee',
+    'button'
   ];
 
   constructor(private shipmentsService: ShipmentsService) { }
 
-
   onLoad() {
+    this.loading = true;
     this.shipmentsService.fetchShipments().subscribe(response => {
+      this.loading = false;
       this.shipmentsArray = response;
     })
   }
 
+
+  onView(event: Event) {
+    console.log(event)
+  }
+
+
+  sortData(sort: Sort) {
+    const data = this.shipmentsArray.slice();
+    console.log(sort)
+    this.shipmentsArray = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'orderNo':
+          return compare(a.orderNo, b.orderNo, isAsc);
+        case 'consignee':
+          return compare(a.consignee, b.consignee, isAsc);
+        case 'customer':
+          return compare(a.customer, b.customer, isAsc);
+        case 'trackingNo':
+          return compare(a.trackingNo, b.trackingNo, isAsc);
+        case 'protein':
+          return compare(a.status, b.status, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
